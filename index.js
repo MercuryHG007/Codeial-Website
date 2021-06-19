94978b233fc4a7f36a9966f76b4923fb5a3b9d07
Harshit Garg <ggharshit1@gmail.com>
2021-06-19 14:06:22 +0530

    ejs and views set

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 // use express router
 app.use('/',require('./routes'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);

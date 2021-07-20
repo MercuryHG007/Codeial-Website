475a01447b49c43ca225b58d40552f7956739171
Harshit Garg <ggharshit1@gmail.com>
2021-07-20 11:24:18 +0530

    convert Post Creation/Deletion to AJAX

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
     }
-
+    
     console.log(`Server is running on port: ${port}`);
 });
